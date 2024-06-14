cede05a287773753da4db25be292495c9a132243
Oleg <logardyt@gmail.com>
2024-06-14 09:51:59 +0700

    poetry.lock

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+import json
+
+
+def transaction_amount(file_path: str) -> list:
+    """Функция, которая выводит сумму транзакции"""
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            repos = json.load(file)
+        if isinstance(repos, list):
+            return repos
+        else:
+            return []
+    except Exception as e:
+        print(f"Ошибка {e}")
+        return []
