c32b837e9daa7e89f2aa3e4a0f6e1e29074d7e5a
Oleg <logardyt@gmail.com>
2024-08-22 13:54:41 +0700

    online store kill2

--- src/Category.py
+++ src/Category.py
@@ -1,19 +1,14 @@
 class Category:
-    categories_set = set()
-    categories_counter = 0
-    products_set = set()
-    products_counter = 0
+    category_count = 0
+    product_count = 0
 
-    def __init__(self, name: str, description: str, products: list):
+    def __init__(self, name: str, description: str, products: list = None) -> None:
         self.name = name
         self.description = description
-        self.__products = products
-        self.full_cost = 0
-        self.prod_number = 0
+        self.products = products
+        Category.category_count += 1
+        Category.product_count += len(self.products)
 
-        Category.categories_set.add(self.name)
-        Category.categories_counter = len(Category.categories_set)
 
-        for x in self.__products:
-            Category.products_set.add(x.name)
-        Category.products_counter = len(Category.products_set)
+
+
